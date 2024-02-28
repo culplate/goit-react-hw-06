620cec7e6f1fc75076432c47cf46a0e44f8d567f
culplate <culplate@gmail.com>
2024-02-28 20:42:44 +0100

    folders fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./Components1/App";
+import App from "./components/App";
 import "./index.css";
 import { Provider } from "react-redux";
 import { persistedStore, store } from "./redux/store";
